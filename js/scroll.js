a025396f3100db1f2be65ce5a49199c2854c9007
Chris Gilbert <chrisgilbert4@icloud.com>
2020-02-01 17:35:10 +0000

    Fade in all secionts

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('#main').fadeIn(1000);
+  $('section').fadeIn(1000);
   $('.scroll').click(function() {
       $('body').animate({
           scrollTop: eval($('#' + $(this).attr('target')).offset().top - 70)
